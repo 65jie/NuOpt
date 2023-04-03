79fe03e764dc42dfd0733ff96aaa132114f48601
65jie <65jiee@gmail.com>
2023-04-04 00:34:31 +0800

    add sample codes

---
+++ README.md
@@ -0,0 +1,2 @@
+# cpp_cmake_gtest_sample
+cpp sample project with cmake and gtest
---
+++ out/build/macos-debug/src/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/peterlau/GitHub/cpp_cmake_gtest_sample/src
+# Build directory: /Users/peterlau/GitHub/cpp_cmake_gtest_sample/out/build/macos-debug/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(SampleTest "/Users/peterlau/GitHub/cpp_cmake_gtest_sample/out/build/macos-debug/SampleTest")
+set_tests_properties(SampleTest PROPERTIES  _BACKTRACE_TRIPLES "/Users/peterlau/GitHub/cpp_cmake_gtest_sample/src/CMakeLists.txt;21;add_test;/Users/peterlau/GitHub/cpp_cmake_gtest_sample/src/CMakeLists.txt;0;")
